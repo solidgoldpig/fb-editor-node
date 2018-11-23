7e15eb46b039c54dc9238dd7c4330c80ce3a1e7d
Alex Robinson <github@alex.fu2k.org>
2018-11-23 16:28:03 +0000

    Add env var to redirect logs

--- bin/start.js
+++ bin/start.js
@@ -5,6 +5,31 @@ const adminRouter = require('../lib/admin/admin')
 
 const postCachedRoutes = () => adminRouter
 
+const path = require('path')
+const fs = require('fs')
+if (process.env.LOGDIR) {
+  const serviceName = process.env.SERVICEDATA.replace(/\/$/, '').replace(/.*\//, '')
+  const logdir = process.env.LOGDIR
+  const accessLogPath = path.join(logdir, `${serviceName}.access.log`)
+  const errorLogPath = path.join(logdir, `${serviceName}.error.log`)
+  try {
+    fs.unlinkSync(accessLogPath)
+  } catch (e) {
+    //
+  }
+  try {
+    fs.unlinkSync(errorLogPath)
+  } catch (e) {
+    //
+  }
+  const accessLog = fs.createWriteStream(accessLogPath, {flags: 'a'})
+  const errorLog = fs.createWriteStream(errorLogPath, {flags: 'a'})
+
+  // redirect stdout / stderr
+  process.__defineGetter__('stdout', () => { return accessLog })
+  process.__defineGetter__('stderr', () => { return errorLog })
+}
+
 server.start({
   postCachedRoutes
 })
